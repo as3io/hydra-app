e1fbbbc12636d3dd0d464f2ccf252bedec33cddf
Josh Worden <solocommand@gmail.com>
2018-06-04 13:41:33 -0500

    change password support

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -4,15 +4,16 @@ import { inject } from '@ember/service';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-import mutation from 'hydra-app/gql/mutations/set-password';
+import SetPassword from 'hydra-app/gql/mutations/set-password';
 
 export default Route.extend(RouteQueryManager, AuthenticatedRouteMixin, {
   flashMessages: inject(),
 
   actions: {
     setPassword(password) {
-      const variables = { input: { password } };
+      const variables = { password };
       const resultKey = 'setPassword';
+      const mutation = SetPassword;
       return this.get('apollo').mutate({ mutation, variables }, resultKey)
         .then(() => this.get('flashMessages').success('Password changed.'))
         .catch(e => this.get('errorProcessor').show(e))
