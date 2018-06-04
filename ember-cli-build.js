ca4c5b360c0a14b07003884c3ad3d4b4f4f06c4f
Josh Worden <solocommand@gmail.com>
2018-06-04 13:53:01 -0500

    remove ion refs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,14 +15,5 @@ module.exports = function(defaults) {
   app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', { destDir: 'assets' });
   app.import('node_modules/bootstrap/dist/css/bootstrap.min.css.map', { destDir: 'assets' });
 
-  app.import('node_modules/ionicons/css/ionicons.min.css');
-  app.import('node_modules/ionicons/css/ionicons.min.css.map', { destDir: 'assets' });
-
-  var ionicons = new Funnel('node_modules/ionicons/fonts', {
-    srcDir: '/',
-    include: [ '*.*' ],
-    destDir: '/fonts'
-  });
-
-  return app.toTree(ionicons);
+  return app.toTree();
 };
